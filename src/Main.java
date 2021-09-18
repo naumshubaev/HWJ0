9be4d5fb0f8d09150baf03f0a324b68df53dcc43
naumshubaev <naum.shubaev@gmail.com>
2021-09-19 00:15:17 +0300

    v1.0 2109190013

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 2_000_000_000 ;
+        int addedSum = 5_000_000;
+        int result;
+        result = amount + addedSum;
+        System.out.println(result);
+    }
+}
